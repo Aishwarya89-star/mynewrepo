ea82980eab796d44d59b0cc4c27277f116dcf21e
Lenovo SSD <Lenovo SSD@192.168.1.33>
2025-04-01 18:45:40 +0530

    my new repo for assignments from super

---
+++ com.assignments/src/main/java/com/assignments/AdditionResult.java
@@ -0,0 +1,13 @@
+package com.assignments;
+
+public class AdditionResult {
+	
+	
+	public void result() {
+
+	
+	int a=10,b=20;
+	int c=a+b;
+	System.out.println(c);
+}
+}
---
+++ com.assignments/src/main/java/com/assignments/ChildResult.java
@@ -0,0 +1,20 @@
+package com.assignments;
+
+public class ChildResult extends AdditionResult {
+	
+	
+	public void display() {
+		
+		
+	super.result();
+	//System.out.println();
+	}
+
+	public static void main(String[] args) {
+		
+		ChildResult child=new ChildResult();
+		child.display();
+		
+	}
+
+}
